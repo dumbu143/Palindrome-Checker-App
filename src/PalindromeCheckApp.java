bdaad1d4d01968e0789c90cd75ada002f37d7fb9
dumbu <dumbunaidu35@gmail.com>
2026-02-25 23:27:11 +0530

    Your commit message

---
+++ src/PalindromeCheckApp.java
@@ -0,0 +1,7 @@
+public class PalindromeCheckApp {
+    public static void main(String[] args){
+        System.out.println("welcome to the palindrome checker management system");
+        System.out.println("version : 1.0");
+        System.out.println("system initialized successfully");
+    }
+}
